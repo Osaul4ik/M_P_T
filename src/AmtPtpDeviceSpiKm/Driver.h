c3191d936168b74c5bcdd38defc79a7b0d19c41e
Vladislav <Osaul4ik@gmail.com>
2026-06-12 15:01:06 +0300

    Update Driver.h

--- src/AmtPtpDeviceSpiKm/Driver.h
+++ src/AmtPtpDeviceSpiKm/Driver.h
@@ -13,6 +13,9 @@ Environment:
     Kernel-mode Driver Framework
 
 --*/
+#define NTDDI_VERSION 0x0A000004
+#define _WIN32_WINNT  0x0A00
+#define WINVER        0x0A00
 
 #include <ntddk.h>
 #include <wdf.h>
