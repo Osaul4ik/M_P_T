44335a55269ee9940adb8f6e75a1e708ad301032
Osaul4ik <osaul4ik@gmail.com>
2026-06-26 03:24:57 +0300

    Debug4

--- src/AmtPtpDeviceUsbKm/Driver.h
+++ src/AmtPtpDeviceUsbKm/Driver.h
@@ -1,16 +1,31 @@
-// Driver definitions. Kernel-mode Driver Framework
-#define _KERNEL_MODE 1
+#pragma once
 
-#include <ntddk.h>
+// 1. Визначаємо версії Windows (вони вже передаються через командний рядок,
+//    але для безпеки продублюємо тут, щоб бути впевненими)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+#ifndef NTDDI_VERSION
+#define NTDDI_VERSION NTDDI_WIN10  // 0xA000010
+#endif
+
+// 2. Включаємо базові заголовки драйвера
+#include <ntddk.h>   // визначає _KERNEL_MODE при /kernel
 #include <wdf.h>
 #include <usb.h>
 #include <usbdlib.h>
 #include <wdfusb.h>
+
+// 3. ЯВНО включаємо wdm.h, щоб отримати прототип KeQueryInterruptTimeToPrecise
+#include <wdm.h>     // <-- ЦЕ КЛЮЧОВИЙ РЯДОК
+
+// 4. Ваші власні заголовки
 #include "device.h"
 #include "queue.h"
 #include "trace.h"
 
-#include <Hid.h>
+// Для HID використовуйте hidport.h замість Hid.h
+#include <hidport.h>
 
 EXTERN_C_START
 
